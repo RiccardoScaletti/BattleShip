e3e8972d7d81ccd4c3c863dac3cc1a9dd13efd3d
Riccardo Scaletti <rickgamedeveloper@gmail.com>
2025-03-08 22:39:11 -0600

    setup

---
+++ ConsoleApp1/Grid.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    enum Directions
+    {
+        Horizontal,
+        Vertical
+    }
+    internal class Grid
+    {
+        string[][] grid = new string[11][];
+
+        Grid()
+        {
+
+        }
+        public void InitializeGrid()
+        {
+            for (int i = 0; i < grid.Length; i++)
+            {
+                grid[i] = new string[grid.Length];
+            }
+
+            grid[0][0] = "0";
+
+            for (int i = 1; i < grid.Length; i++)
+            {
+                grid[0][i] = i.ToString();
+                grid[i][0] = i.ToString();
+            }
+
+            for (int row = 1; row < grid.Length; row++)
+            {
+                for (int col = 1; col < grid.Length; col++)
+                {
+                    grid[row][col] = "";
+                }
+            }
+        }
+
+        public void FillGrid()
+        {
+            for (int row = 1; row < grid.Length; row++)
+            {
+                for (int col = 1; col < grid.Length; col++)
+                {
+                    if (grid[row][col] == "")
+                    {
+                        grid[row][col] = ('~').ToString();
+                    }
+                }
+            }
+        }
+
+        public void DisplayGrid()
+        { 
+            for (int row = 0; row < grid.Length; row++)
+            {
+                for (int col = 0; col < grid.Length; col++)
+                {
+                    Console.Write(grid[row][col].PadRight(3));
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void PlaveShip(int x, int y)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Battleship
+{
+    internal class Program
+    {
+        static bool aiOn = false;
+        static bool stop = false;
+        static string? playerModeInput;
+        static void Main(string[] args)
+        {
+            Grid playerGrid = new Grid();
+            Grid enemyGrid = new Grid();
+
+            Console.WriteLine("Press any key to start.... \n");
+            Console.ReadKey();
+            Console.WriteLine("\n");
+            //do
+            //{
+            //    Console.WriteLine("do You want to play against AI or Player? \n A = Ai, P = player");
+            //    playerModeInput = Console.ReadLine();
+            //    if (playerModeInput == "A")
+            //    {
+            //        aiOn = true;
+            //        stop = true;
+            //    }
+            //    else if (playerModeInput == "P")
+            //    {
+            //        aiOn = false;
+            //        stop = true;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Wrong input, try again...\n");
+            //    }
+            //}while(!stop);
+
+            DiplayBoard(playerGrid, enemyGrid);
+
+        }
+
+        static void DiplayBoard(Grid pg, Grid eg)
+        {
+            Console.WriteLine("\tPlayer Grid");
+            eg.InitializeGrid();
+            eg.FillGrid();
+            eg.DisplayGrid();
+
+            Console.WriteLine("\tEnemy Grid");
+            pg.InitializeGrid();
+            pg.FillGrid();
+            pg.DisplayGrid();
+        }
+    }
+}
